df3e7c78596940f313ae4402823a3386a87e9355
Siti Fatimah <ifanasz@gmail.com>
2017-01-17 21:17:53 +0700

    updated register page

---
+++ resources/views/layouts/layout_register.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Marketinc - Register</title>
+
+    <!-- Styles -->
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+</head>
+<body>
+    <header>
+        <img src="{{ asset("/image/header.png")}}" style="width:100%;">
+        <div class="yellow-spacing" style="background-color:#ffcc46;width:100%;height:25%;text-align:center;">
+            <img src="{{ asset("/image/comp.png")}}" style="width:300px;height:auto;">
+        </div>
+    </header>
+    <section="content">
+        @yield('content')
+    </section>
+
+    <!-- Scripts -->
+    <script src="/js/app.js"></script>
+</body>
+</html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Register Marketinc</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>  
-  </head>
+@extends('layouts.layout_register')
 
-  <body>
-      <img src="{{ asset("/image/header.png")}}" style="width:100%;">
-  </body>
-</html>
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
