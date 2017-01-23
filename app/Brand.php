0953f616c63c6e356c01f842fe7869cc669d4e8e
kevinega <kevin.ega@ui.ac.id>
2017-01-23 20:25:36 +0700

    Change Model and .env for reset password support

--- app/Brand.php
+++ app/Brand.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticable;
+
 class Brand extends Authenticable
 {
+
+	use Notifiable;
+
     protected $fillable = [
         'brand_name', 'username', 'address', 'location', 'phone_one', 'phone_two', 'email', 'password', 'membership', 'description', 'logo', 'cover', 'open_hour',
     ];
