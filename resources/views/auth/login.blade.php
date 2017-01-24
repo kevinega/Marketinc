123ea4d5e8dc8d7b2274edd29ef76e88414a5b43
Hasandi Patriawan <sandi.patriawan@gmail.com>
2017-01-24 19:37:45 +0700

    Fixed password input type

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
         {!! Form::open(['id' => 'login-route-form']) !!}
             <img src="/img/logo-full.png" class="mx-auto d-block" alt="responsive_image">
             {!! Form::text('username', old('username'), ['id' => 'username', 'class' => 'form-control margin-bottom-10', 'placeholder' => 'Username', 'required']) !!}
-            {!! Form::text('password', old('username'), ['id' => 'password', 'class' => 'form-control', 'placeholder' => 'Password', 'required']) !!}
+            {!! Form::password('password', ['id' => 'password', 'class' => 'form-control', 'placeholder' => 'Password', 'required']) !!}
             <a id="forgot-password" class="margin-bottom-10" href="{{ url('/password/reset') }}">Forgot Password?</a>    
             {!! Form::button('Log in', ['class' => 'btn btn-primary btn-block margin-bottom-10', 'type' => 'submit']) !!}
             <a id="register-question" href='{{ url('/register') }}'>Don't have any account? Join now!</a>
