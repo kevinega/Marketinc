c80effdf71aeefca4850d5d3c4106af6f28f89f6
Siti Fatimah <ifanasz@gmail.com>
2017-02-07 10:48:11 +0700

    delete navbar registration form

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,8 +1,5 @@
 {{-- navbar register page --}}
 <header>
-	<div class="navbar">
-		<a href='{{ url('/login') }}'>Log In</a>
-	</div>
 	<div class="yellow-spacing">
 		<img src="{{ asset("/img/marketinc.gif")}}" loop="-1">
 	</div>
