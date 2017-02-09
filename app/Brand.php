3ee0c8ea3508aa934563368b8f6588f8c868140e
rama rahmatullah <ramarahmatullah6@gmail.com>
2017-02-09 21:30:52 +0700

    fixed register

--- app/Brand.php
+++ app/Brand.php
@@ -12,7 +12,7 @@ class Brand extends Authenticable
 	use Notifiable;
 
     protected $fillable = [
-        'brand_name', 'username', 'address', 'location', 'phone_one', 'phone_two', 'email', 'password', 'membership', 'description', 'logo', 'cover', 'open_hour', 'confirmed', 'confirmation_code',
+        'brand_name', 'username', 'address', 'phone_one', 'phone_two', 'email', 'password', 'membership', 'description', 'logo', 'cover', 'open_hour', 'confirmed', 'confirmation_code', 'music',
     ];
 
     protected $hidden = [
