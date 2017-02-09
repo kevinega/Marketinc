5e4bed254d230a4f779aaf16f6ae5b932e4e6212
kevinega <kevin.ega@ui.ac.id>
2017-02-09 21:17:30 +0700

    add foreign key brand_id to transaction that refers to id on brands.

---
+++ database/migrations/2017_02_09_131229_add_brand-id_transaction.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBrandIdTransaction extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('transactions', function (Blueprint $table) {
+            $table->unsignedInteger('brand_id')->nullable();
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       
+    }
+}
