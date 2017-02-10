21ee66b9375496c3ffaef70a12444bcbae4e78a3
Hasandi Patriawan <sandi.patriawan@gmail.com>
2017-02-10 18:08:31 +0700

    Updated the navbar to be more universal

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,12 @@
-{{-- navbar register page --}}
 <nav class="navbar navbar-inverse fixed-top bg-inverse">
 	<div class="container-nav">
 		<a href="{{ url('/') }}" class="navbar-brand"><img src="{{ elixir('img/favicon.png') }}" alt=""></a>
+		@if(Auth::guard()->check())
+		<a href="">Hi, <strong>{{ Auth::guard()->user()->name }}</strong></a>
+		@elseif(Auth::guard('admin_users')->check())
+		<a href="">Hi, <strong>{{ Auth::guard('admin_users')->user()->name }}</strong></a>
+		@else
 		<a href='{{ url('/login') }}'>Log In</a>
+		@endif
 	</div>
 </nav>
