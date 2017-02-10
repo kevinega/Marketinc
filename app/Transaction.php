6dedc0782a709a11b02594e1b935c021ccd26bc7
kevinega <kevin.ega@ui.ac.id>
2017-02-10 15:49:11 +0700

    [FIX] Transaction id return 0

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
+
+		public $incrementing = false;
      	public function brand(){
      		return $this->belongsTo('App\Brand');
      	}
--- resources/views/admin-home.blade.php
+++ resources/views/admin-home.blade.php
@@ -24,6 +24,7 @@
             <thead>
                 <tr>
                     <th>Brand Name</th>
+                    <th>Transaction ID</th>
                     <th>Transaction Added</th>
                     <th>Valid Until</th>
                     <th>Confirmation Code</th>
@@ -36,12 +37,14 @@
             <tbody>
                 @foreach ($transactions as $transaction)
                 <tr>
+                    
                     <td>{{ $transaction->brand->brand_name }}</td>
+                    <td>{{ $transaction->id }}</td>
                     <td>{{ $transaction->created_at->format('F d, Y h:ia') }}</td>
                     <td>{{ $transaction->brand->valid_until }}</td>
                     <td>{{ $transaction->confirmation_code }}</td>
                     <td>{{ $transaction->flag }}</td>
-                    <td>{{ $transaction->total_payment }}
+                    <td>{{ $transaction->total_payment }}</td>
                     <td>
                         <a href="/admin/approve/{{ $transaction->id }}" class="btn btn-info pull-left" style="margin-right: 3px;">Approve</a>
                         <a href="/admin/delete/{{ $transaction->id }}" class="btn btn-danger pull-left" style="margin-right: 3px;">Delete</a>
