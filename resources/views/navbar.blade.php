81021a609fafa28749aaf807681fd9245e14ff35
Siti Fatimah <ifanasz@gmail.com>
2017-02-21 00:11:21 +0700

    updated details and facilities view

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -10,3 +10,5 @@
 		@endif
 	</div>
 </nav>
+
+<div class="navbar-seperation"></div>
--- resources/views/sections/detail.blade.php
+++ resources/views/sections/detail.blade.php
@@ -1,1 +1,42 @@
-detail
+<div class="feature-header">
+	<h4>DETAILS</h4>
+	<i class="material-icons btn-edit">mode_edit</i>
+</div>
+
+<div class="feature-details-about">
+    <div class="maps">
+    	<iframe width="100%" height="400px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJweaD5rvzaS4RkWhE7KiP38Y&key=AIzaSyDcfHgRTp-c6n4lachCjzNi73DDY5GFdag" allowfullscreen></iframe>
+    </div>
+    <div class="about-us" style="border:1px solid grey;">
+        <div class="description">
+        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div class="location">
+        	<p>
+        		Jl. MT Haryono Kav 8 No. 9, TIS Square</br>
+        		021-4891644</br>
+        		<strong>Open Hours:</strong> 08.00 - 22.00
+        	</p>
+        </div>
+    </div>
+</div>
+<div class="feature-details-facilities">
+	<div class="facilities-flex">
+		<div class="facility"><img src="{{ asset("img/facilities/breakfast.png") }}"> Breakfast</div>
+		<div class="facility"><img src="{{ asset("img/facilities/smoking-area.png") }}"> Smoking Area</div>
+		<div class="facility"><img src="{{ asset("img/facilities/reservation.png") }}"> Reservation</div>
+		<div class="facility"><img src="{{ asset("img/facilities/valet.png") }}"> Valet</div>
+	</div>
+	<div class="facilities-flex">
+		<div class="facility"><img src="{{ asset("img/facilities/parking-lot.png") }}"> Parking Lot</div>
+		<div class="facility"><img src="{{ asset("img/facilities/ac.png") }}"> Air Conditioner</div>
+		<div class="facility"><img src="{{ asset("img/facilities/private-room.png") }}"> Private Room</div>
+		<div class="facility"><img src="{{ asset("img/facilities/delivery.png") }}"> Delivery</div>
+	</div>
+	<div class="facilities-flex">
+		<div class="facility"><img src="{{ asset("img/facilities/wifi.png") }}"> WiFi</div>
+		<div class="facility"><img src="{{ asset("img/facilities/working-space.png") }}"> Working Space</div>
+		<div class="facility"><img src="{{ asset("img/facilities/alcohol.png") }}"> Alcohol</div>
+		<div class="facility"><img src="{{ asset("img/facilities/changing-room.png") }}"> Changing Room</div>
+	</div>
+</div>
