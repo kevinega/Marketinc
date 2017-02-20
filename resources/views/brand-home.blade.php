c7c5abcb756cd31010a3337719e68eeb91e351df
Siti Fatimah <ifanasz@gmail.com>
2017-02-20 15:28:02 +0700

    updated upload logo style

--- resources/views/brand-home.blade.php
+++ resources/views/brand-home.blade.php
@@ -1,49 +1,67 @@
 @extends('layouts.app')
 
 @section('page-style')
-<link href="{{ elixir('css/navbar.css') }}" rel="stylesheet">
-{{-- <link href="{{ elixir('css/brand-home.css') }}" rel="stylesheet"> --}}
+<link href="{{ elixir('css/brand-home.css') }}" rel="stylesheet">
 @endsection
 
 @section('navbar')
-  @include('navbar')
+    @include('navbar')
 @endsection
 
 @section('content')
+    <div class="feature-photo">
+        <!-- display picture -->
+        <?php $path = Auth::guard()->user()->logo; ?>
+        <img src="{{ asset("storage/$path") }}" class="photo-360">
+
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#upload-logo-modal">
+          <i class="material-icons">add_a_photo</i>
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="upload-logo-modal" tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modal-label">Upload Logo</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                  {!! Form::open(['method' => 'post', 'url' => 'brand/upload', 'enctype' => 'multipart/form-data']) !!}
+                  {{ csrf_field() }}
+
+                  {!! Form::file('logo', ['class' => 'upload-logo']) !!}
+                  {!! Form::button('Save Logo', ['class' => 'btn btn-primary btn-block', 'type' => 'submit']) !!}
+                  {!! Form::close() !!}
+              </div>
+            </div>
+          </div>
+        </div>
+    </div> 
+
     <div class="container">
-        <div class="feature-flex">
-
-            <!-- display picture -->
-            <?php $path = Auth::guard()->user()->logo; ?>
-            <img src="{{ asset("storage/$path") }}">
-
-            {!! Form::open(['method' => 'post', 'url' => 'brand/upload', 'enctype' => 'multipart/form-data']) !!}
-            {{ csrf_field() }}
-
-            {!! Form::file('logo') !!}
-            {!! Form::button('Save Logo', ['class' => 'btn btn-primary btn-block', 'type' => 'submit']) !!}
-            {!! Form::close() !!}
-            
-        </div> 
-        <div class="feature-flex">
+        <div class="feature">
             @include('sections.album')
         </div>
-        <div class="feature-flex">
+        <div class="feature">
             @include('sections.article')
         </div>
-        <div class="feature-flex">
+        <div class="feature">
             @include('sections.detail')
         </div>
-        <div class="feature-flex">
+        <div class="feature">
             @include('sections.map')
         </div>
-        <div class="feature-flex">
+        <div class="feature">
             @include('sections.music')
         </div>
-        <div class="feature-flex">
+        <div class="feature">
             @include('sections.promotion-list')
         </div>
-        <div class="feature-flex">
+        <div class="feature">
             @include('sections.video')
         </div>
     </div>
