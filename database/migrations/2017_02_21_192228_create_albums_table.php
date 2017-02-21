9aa256e434a78b76fcc759b79663f64724c54855
rama rahmatullah <ramarahmatullah6@gmail.com>
2017-02-22 02:29:23 +0700

    create tabel album yang berelasi ke brands

---
+++ database/migrations/2017_02_21_192228_create_albums_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('brand_id')->unsigned();
+            $table->string('name');
+            $table->string('desc');
+            $table->timestamps();
+        });
+
+        Schema::table('albums',function(Blueprint $table) {
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
