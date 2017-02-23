002ec61fdf8b926602ac45fc45d3c8db1a1a21b7
Hasandi Patriawan <sandi.patriawan@gmail.com>
2017-02-23 19:08:34 +0700

    Updated brand management page

--- resources/views/admin/brand-management.blade.php
+++ resources/views/admin/brand-management.blade.php
@@ -9,43 +9,50 @@
 @endsection
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
+
+    @php
+    $url = url()->current();
+    if (Request::input('sort','asc') == 'asc') {
+        $sort='desc';
+    } else {
+        $sort='asc';
+    }
+    @endphp
+
     <div class="row">
-        <div class="col-lg-10 col-lg-offset-1">
+        <div class="col-12">
+            <h1><i class="fa fa-users"></i>Brand Management</h1>    
+        </div>
+    </div>
 
-@php
-$url = url()->current();
-if(Request::input('sort','asc') == 'asc'){
-    $sort='desc';
-}else{
-    $sort='asc';
-}
-@endphp
+    <div class="row">
+        <div class="col-12">
+            {!! Form::open(['method'=>'GET','url'=> $url,'class'=>'search-bar','role'=>'search'])  !!}
+            <div class="input-group custom-search-form">
+                <input type="text" class="form-control" name="search" placeholder="Search...">
+                <span class="input-group-btn">
+                    <button class="btn btn-default-sm" type="submit">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </span>
+            </div>
+            {!! Form::close() !!}
 
-{!! Form::open(['method'=>'GET','url'=> $url,'class'=>'navbar-form navbar-left','role'=>'search'])  !!}
-<div class="input-group custom-search-form">
-    <input type="text" class="form-control" name="search" placeholder="Search...">
-    <span class="input-group-btn">
-        <button class="btn btn-default-sm" type="submit">
-            <i class="fa fa-search"></i>
-        </button>
-    </span>
-</div>
-{!! Form::close() !!}
-            <h1><i class="fa fa-users"></i>Brand Management</h1>
             @if($brands->total() == 0)
-                BRAND NOT FOUND
-            @endif
+            <div class="alert alert-danger" role="alert">
+                <strong>Oh snap!</strong> Brand not found. You may try another keyword.
+            </div>
+            @else
             <div class="table-responsive">
-                <table class="table table-bordered table-striped">
-
-                    <thead>
+                <table class="table table-sm table-striped table-hover">
+                    <thead class="thead-inverse">
                         <tr>
-                            <th>Brand Name<a href="{{url('unicorn?orderBy=brand_name&sort=')}}{{$sort}}">sort</a></th>
-                            <th>Valid Until<a href="{{url('unicorn?orderBy=valid_until&sort=')}}{{$sort}}">sort</th>
-                            <th>Status<a href="{{url('unicorn?orderBy=status&sort=')}}{{$sort}}">sort</th>
-                            <th>Membership<a href="{{url('unicorn?orderBy=membership&sort=')}}{{$sort}}">sort</th>
-                            <th>Email is Verified<a href="{{url('unicorn?orderBy=verified&sort=')}}{{$sort}}">sort</th>
+                            <th>Brand Name<a href="{{url('unicorn?orderBy=brand_name&sort=')}}{{$sort}}"><i class="fa fa-fw fa-sort"></i></a></th>
+                            <th>Valid Until<a href="{{url('unicorn?orderBy=valid_until&sort=')}}{{$sort}}"><i class="fa fa-fw fa-sort"></i></th>
+                            {{-- <th>Status<a href="{{url('unicorn?orderBy=status&sort=')}}{{$sort}}"><i class="fa fa-fw fa-sort"></i></th> --}}
+                            <th>Membership<a href="{{url('unicorn?orderBy=membership&sort=')}}{{$sort}}"><i class="fa fa-fw fa-sort"></i></th>
+                            <th>Email Verification<a href="{{url('unicorn?orderBy=verified&sort=')}}{{$sort}}"><i class="fa fa-fw fa-sort"></i></th>
                             <th></th>
                         </tr>
                     </thead>
@@ -54,29 +61,34 @@ if(Request::input('sort','asc') == 'asc'){
                         @foreach ($brands as $brand)
                         <tr>
                             <td>{{ $brand->brand_name }}</td>
-                            <td>{{ $brand->valid_until }}</td>
-                            <td>{{ $brand->status }}</td>
-                            <td>{{ $brand->membership }}</td>
-                            <td>
-                                @if($brand->verified)
-                                    Verified
+                            <td class="center">
+                                @if ($brand->valid_until=='0000-00-00 00:00:00')
+                                -
                                 @else
-                                Not Verified
+                                {{ date('d F Y', strtotime($brand->valid_until)) }}
+                                @endif
+                            </td>
+                            {{-- <td>{{ $brand->status }}</td> --}}
+                            <td class="center uppercase">{{ $brand->membership }}</td>
+                            <td class="center">
+                                @if($brand->verified)
+                                <span class="badge badge-primary">VERIFIED</span>
+                                @else
+                                <span class="badge badge-default">NOT VERIFIED</span>
                                 @endif
                             </td>
                             <td>
-                                <a href="/unicorn/reset/{{ $brand->id }}" class="btn btn-danger pull-left" style="margin-right: 3px;">Reset Membership</a>
+                                <a href="/unicorn/reset/{{ $brand->id }}" class="btn btn-primary btn-block btn-sm" style="margin-right: 3px;"><strong>Reset Membership</strong></a>
                             </td>
                         </tr>
                         @endforeach
                     </tbody>
-
                 </table>
-                {{ $brands->links()}}
             </div>
+            {{ $brands->links()}}
+            @endif
+
         </div>
-
-
     </div>
 </div>
 @endsection
