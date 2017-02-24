f17fca9da34f980c40ebd23c2319add4dea49b0d
kevinega <kevin.ega@ui.ac.id>
2017-02-24 17:32:26 +0700

    fix navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-inverse fixed-top bg-inverse">
 	<div class="container-nav">
 		<a href="{{ url('/') }}" class="navbar-brand"><img src="{{ elixir('img/favicon.png') }}" alt=""></a>
-		@if(Auth::guard()->check())
+		@if(Auth::guard()->check() && !Request::is('unicorn*'))
 		<a href="#">Hi, <strong>{{ Auth::guard()->user()->brand_name }}</strong></a>
 		@elseif(Auth::guard('admin_users')->check() && Request::is('unicorn*'))
 		<a href="#">Hi, <strong>{{ Auth::guard('admin_users')->user()->name }}</strong></a>
