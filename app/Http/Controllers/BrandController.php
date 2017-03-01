6ce607000dd37411cae0a22366f026f6c08c5796
kevinega <kega.pratama@gmail.com>
2017-03-01 20:12:11 +0700

    Fix brandcontroller middleware to auth_brand (refix after last merge)

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -26,7 +26,7 @@ class BrandController extends Controller
     
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth_brand');
     }
 
     /**
