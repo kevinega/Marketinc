79cc2d9ebcd8c7074ab61cd11d377d603933c103
kevinega <kega.pratama@gmail.com>
2017-03-10 16:00:13 +0700

    new migration: alter table article add column description, drop column published_on

---
+++ database/migrations/2017_03_10_085159_alter_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('description');
+            $table->dropColumn('published_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn('description');
+            $table->date('published_on');
+        });
+    }
+}
