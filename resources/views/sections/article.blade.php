5a26e84064787d11d875cb04f4b0360fc8061bf1
Hasandi Patriawan <sandi.patriawan@gmail.com>
2017-03-10 21:20:30 +0700

    Code restructure on article

--- resources/views/sections/article.blade.php
+++ resources/views/sections/article.blade.php
@@ -1,21 +1,20 @@
-<div id='articles'><h2>Articles</h2></div>
 <div>
-		{!! Form::open(['url' => 'brand/article/create', 'method' => 'POST']) !!}
+  <h2>Articles</h2>
+  <button class="btn btn-primary btn-sm"><i class="fa fa-plus fa-fw"></i>Add article</button>
+</div>
+<div id='articles'>
 
-        {!! Form::label('url', 'Paste or write the link (URL) of the article: ') !!}</br>
-        {!! Form::text('url', null, ['id' => 'urlKu', 'class' => 'form-control', 'placeholder' => 'ex: http://www.facebook.com or https://www.facebook.com']) !!}
-        </br>
-
-        {!! Form::label('title', 'Title: ') !!}</br>
-        {!! Form::text('title', null, ['id' => 'title', 'class' => 'form-control', 'placeholder' => 'Original title will be used if this field is left empty']) !!}</br>
-        {!! Form::label('author', 'Author name: ') !!}</br>
-
-        {!! Form::text('author', null, ['id' => 'author', 'class' => 'form-control', 'placeholder' => 'Original author name will be used if this field is left empty']) !!}</br>
-
-        {!! Form::label('description', 'Article\'s description: ') !!}</br>
-        {!! Form::text('description', null, ['id' => 'description', 'class' => 'form-control', 'placeholder' => 'Original Description name will be used if this field is left empty']) !!}</br>
-
-        {!! Form::button('Submit Article', ['class' => 'btn btn-primary', 'type' => 'submit']) !!}
-
-        {!! Form::close() !!}
+</div>
+<div>
+  {!! Form::open(['url' => 'brand/article/create', 'method' => 'POST']) !!}
+  {!! Form::label('url', 'URL') !!}
+  {!! Form::text('url', null, ['id' => 'urlKu', 'class' => 'form-control', 'placeholder' => 'ex: http://www.facebook.com or https://www.facebook.com']) !!}
+  {!! Form::label('title', 'Title') !!}
+  {!! Form::text('title', null, ['id' => 'title', 'class' => 'form-control', 'placeholder' => 'Original title will be used if this field is left empty']) !!}
+  {!! Form::label('author', 'Author') !!}
+  {!! Form::text('author', null, ['id' => 'author', 'class' => 'form-control', 'placeholder' => 'Original author name will be used if this field is left empty']) !!}
+  {!! Form::label('description', 'Description') !!}
+  {!! Form::text('description', null, ['id' => 'description', 'class' => 'form-control', 'placeholder' => 'Original Description name will be used if this field is left empty']) !!}
+  {!! Form::button('Submit article', ['class' => 'btn btn-primary', 'type' => 'submit']) !!}
+  {!! Form::close() !!}
 </div>
