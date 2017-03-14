c96b67ea0af915d174783ffbcddda9a568625e68
kevinega <kega.pratama@gmail.com>
2017-03-14 15:29:13 +0700

    Model relation setup for details (facilities)

--- app/Brand.php
+++ app/Brand.php
@@ -34,4 +34,8 @@ class Brand extends Authenticable
     {
         $this->notify(new ResetPasswordNotification($token));
     }
+
+    public function facility(){
+        return $this->hasMany('App\Facility');
+    }
 }
