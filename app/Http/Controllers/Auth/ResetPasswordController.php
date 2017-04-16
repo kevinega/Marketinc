cdd1ec6638e1acf1b181bb7390f13b5c48a014f3
kevinega <kega.pratama@gmail.com>
2017-04-16 20:58:36 +0700

    Set rules for reset password form
    min password 6, max password 25

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -36,4 +36,13 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',x
+            'password' => 'required|min:6|max:25|confirmed',
+        ];
+    }
 }
